13f793f2f520e4c636c548609f68f4360025fe0d
Alex Ivannikov <polkil0@yandex.ru>
2021-12-23 02:06:15 +0700

    создал ветку для lesson11

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+const btn = document.getElementById('btn');
+const text = document.getElementById('text');
+const square = document.getElementById('square');
+const eBtn = document.getElementById('e_btn');
+const range = document.getElementById('range');
+const circle = document.getElementById('circle');
+
+btn.addEventListener('click', function() {
+    square.style.backgroundColor = text.value;
+    
+});
+
+eBtn.style.display = 'none';
+
+range.addEventListener('input', function() {
+    circle.style.width = range.value + '%';
+    circle.style.height = range.value + '%';
+});
+
+
 
 
 
